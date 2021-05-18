6a5211a9668f38e0fb59bdaa72be9fdae442f222
Aparna <aparnasinghktr125@gmail.com>
2021-05-19 00:27:14 +0530

    login-logout

--- display/urls.py
+++ display/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
 	path("", views.home_page,name="home"),
+	path("register/",views.register,name="register"),
+	path("login/",views.login,name="login"),
+	path("logout/",views.logout,name="logout"),
+	path("aboutus/",views.about,name="about"),
 	path("rental/",views.rental,name="rental"),
 	path("tiffin/",views.tiffin,name="tiffin"),
 	path("miscellaneous",views.misc,name="misc"),
--- display/views.py
+++ display/views.py
@@ -1,5 +1,7 @@
 from django import http
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,redirect
+from django.contrib.auth.models import User,auth
+from django.contrib import messages
 from .models import user_query,hostel
 # Create your views here.
 def home_page(request):
@@ -13,6 +15,57 @@ def home_page(request):
             query.save()
             return render(request,"display/index.html")  
     return render(request,"display/index.html")
+
+def register(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        username = request.POST['username']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+
+        if pass1==pass2:
+            if User.objects.filter(username=username).exists():
+                messages.info(request,'Username already in use!')
+                return redirect('/register/')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request,'Your email id already exists!')
+                return redirect('/register/')
+            else:        
+                user = User.objects.create_user(first_name=name,username=username, email=email,password = pass1)
+                user.save();
+                return redirect('/login/')
+                
+        else:
+            messages.info(request,"Password doesn't match") 
+            return redirect('/register/')       
+           
+
+    else:
+        return render(request,"display/register.html") 
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username,password=password)
+        if user is not None:
+            auth.login(request,user)
+            return redirect("/")
+        else:
+            messages.info(request,"Invalid Credentials!")
+            return redirect('/login/')    
+    else:    
+        return render(request,"display/login.html")    
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')  
+
+def about(request):
+    return render(request,"display/aboutus.html")          
+
 
 def rental(request):
     if request.method == "GET":
