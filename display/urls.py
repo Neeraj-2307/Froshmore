465500adc7210aa2e1612b040f5c19bdfab31218
Aparna <aparnasinghktr125@gmail.com>
2021-05-19 14:16:52 +0530

    Proper validation in register

--- display/urls.py
+++ display/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
 	path("rental/",views.rental,name="rental"),
 	path("tiffin/",views.tiffin,name="tiffin"),
 	path("miscellaneous",views.misc,name="misc"),
+	path("register/",views.register,name="register"),
+	path("login/",views.login,name="login"),
+	path("logout/",views.logout,name="logout"),
 ]
